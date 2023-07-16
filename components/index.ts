330ad7e45c32bf399c23b4ece2992f12928d1975
waiyanhtun282 <waiyanhtun282@gmail.com>
2023-07-16 09:59:46 +0630

    first commit

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import CustomButton from './CustomButton';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufactuer';
+import CarCard from './CarCard';
+import CustomerFilter from './CustomerFilter';
+import ShowMore from './ShowMore';
+ export {
+  Hero,
+  CustomButton, 
+  NavBar,
+  SearchBar, 
+ SearchManufacturer,
+ CarCard,
+ CustomerFilter,
+ ShowMore,
+  Footer,
+}
